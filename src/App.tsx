2be5c7aa8961786732c247d12e688af71c111651
uriellevy <uriellevy4892@gmail.com>
2022-05-15 12:28:56 +0300

    added app+styling component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
+import HomeView from './components/HomeView';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>hello</h1>
-    </div>
-  );
+ export interface Item {
+  id: number
+  title: string
+  completed: boolean
 }
 
-export default App;
+interface AppState {
+  todos: Item[]
+  search: string
+}
+
+export default class App extends React.Component<{}, AppState> {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: "homework",
+        completed: false
+      },
+      {
+        id:2,
+        title: "walk",
+        completed: false
+      },
+      {
+        id:3,
+        title: "swim",
+        completed: false
+      }
+    ],
+    search: ''
+  }
+
+  
+  render() {
+    console.log(this.state.todos)
+    const {todos, search} = this.state;
+    return (
+      <div className='App'>
+        <div className='wrapper'>
+          <HomeView/>
+        </div>
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Todos.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Todos extends Component {
+  render() {
+    return (
+      <div>Todos</div>
+    )
+  }
+}
